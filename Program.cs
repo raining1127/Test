c02a724f9ed80c5dd8e9f0faa1a811eb62735be0
raining1127 <raining1127@qq.com>
2013-10-11 23:41:24 +0800

    console application

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootElement = XElement.Load("test.xml");
+            var nameNodes = rootElement.Descendants(rootElement.Name.Namespace + "Name");
+
+            foreach (var nameNode in nameNodes)
+            {
+                Console.WriteLine(nameNode.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
